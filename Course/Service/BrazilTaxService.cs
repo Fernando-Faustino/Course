710b61c3847220682578ab41a990288bdec8e7c4
Fernando-Faustino <fernandofaustino627@gmail.com>
2023-05-20 17:47:31 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Service/BrazilTaxService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Service {
+    internal class BrazilTaxService : ITaxService{
+
+        public double Tax (double amount) {
+
+            if (amount <= 100.0) {
+                return amount * 0.2;
+            }
+            
+            else {
+                return amount * 0.15;
+            }
+         }
+    }
+}
+
